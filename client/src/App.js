90daed9f7247b09651a0c8dd6bf0c2225172c9d4
erreinoso <eramirezreinoso@gmail.com>
2021-01-29 16:43:41 +0100

    build: landing and filter html styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import './stylesheets/App.css';
+import React from 'react';
+import Filter from './components/Filter';
+import './stylesheets/App.scss';
+import Logo from './images/logo.png';
 
-function App() {
-  return (
-    <div className="text-center">
-      <header className="App-header">
-        <h1>holi</h1>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App--header">
+            <h1 className="hidden">Pet names generator</h1>
+            <div className="logo">
+                  <img className="logo-image" src={Logo} alt="Logo" />
+            </div>
+          </header>
+          <section className="App--section">
+          <div className="App--newName"></div>
+        <div className="App--button">
+          <button className="App--button__inner ">generar nombre</button>
+        </div>     
+          </section>
+
+            <Filter/>
+
+        </div>
+      );
+    }
+  }
 
 export default App;
